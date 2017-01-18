d13a0be19bd06a4042e4dc9875a6e60fa879ae09
zhou3388232 <2482891006@qq.com>
2017-01-18 17:36:00 +0800

    nav

--- resources/views/base/footer.blade.php
+++ resources/views/base/footer.blade.php
@@ -2,5 +2,19 @@
         <div class="wheel"></div>
       </div>
       <script type="text/javascript" src="{{ asset('/js/main.js') }}"></script>
+<script type="text/javascript">
+(function($) {
+    $(function() {
+        var c = "{{getCurrentControllerName()}}";
+        $('.page-left .main-nav li a').removeClass('active');
+        console.log(c);
+        if(c != 'user' && c != 'setting'){
+            $('.page-left .main-nav li.admin a').addClass('active');
+        }else{
+            $('.page-left .main-nav li.'+ c +' a').addClass('active');
+        }
+    });
+})(jQuery);
+</script>
   </body>
 </html>
