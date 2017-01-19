d29a40d903cb8603af2febd0babfe0dd2a4e5117
zhou3388232 <2482891006@qq.com>
2017-01-19 15:16:47 +0800

    pageination handle

--- resources/views/base/pagination.blade.php
+++ resources/views/base/pagination.blade.php
@@ -9,7 +9,7 @@
   parse_str($_SERVER['QUERY_STRING'], $_params);
 ?>
 <div class="pagination clearfix">
-@if(isset($totalpage) && $totalpage != 0)
+@if(isset($totalpage) && $totalpage > 10)
   <div class="container fl">
     @if($_page > 1)
       <?php
@@ -49,7 +49,6 @@
       <a href="{{$_base}}?{{$_query}}">{{$i+3}}</a>
       @endif
     @else
-      
     @endif
     <?php } ?>
     @if($_page < $totalpage)
@@ -70,6 +69,47 @@
     <a class="page-jump btn" href="#">跳页</a>
     @if(isset($totalpage)) {{$totalpage}} @else 0 @endif
   </div>
+@else
+  <div class="container fl">
+    @if($_page > 1)
+      <?php
+        $_params['page'] = $_page-1;
+        $_query = http_build_query($_params);
+      ?>
+      <a class="page-prev" href="{{$_base}}?{{$_query}}"><i class="iconfont icon-prev"></i></a>
+    @else
+      <a class="page-prev" href="#"><i class="iconfont icon-prev"></i></a>
+    @endif
+
+    <?php for($i=1; $i <= $totalpage; $i++){ ?>
+    <?php
+      $_params['page'] = $i;
+      $_query = http_build_query($_params);
+    ?>
+    @if($i == $_page)
+      <span class="current">{{$i}}</span>
+    @else
+      <a href="{{$_base}}?{{$_query}}">{{$i}}</a>
+    @endif
+    <?php } ?>
+    @if($_page < $totalpage)
+      <?php
+        $_params['page'] = $_page+1;
+        $_query = http_build_query($_params);
+      ?>
+      <a class="page-next" href="{{$_base}}?{{$_query}}"><i class="iconfont icon-next"></i></a>
+    @else
+      <a class="page-next" href="#"><i class="iconfont icon-next"></i></a>
+    @endif
+
+    <span>转到</span>
+    <select>
+      <option>1</option>
+    </select>
+    <span>页</span>
+    <a class="page-jump btn" href="#">跳页</a>
+    @if(isset($totalpage)) {{$totalpage}} @else 0 @endif
+    </div>
 @endif
   <div class="sum-info fr">
     <span>共@if(isset($total)) {{$total}} @else 0 @endif位注册用户</span>
