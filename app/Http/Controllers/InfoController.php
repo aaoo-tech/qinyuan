ea3a571cbd734c880cdd7c3eb9ca4a1d666196aa
zhou3388232 <2482891006@qq.com>
2017-01-20 11:57:34 +0800

    info profile history

--- app/Http/Controllers/InfoController.php
+++ app/Http/Controllers/InfoController.php
@@ -9,15 +9,13 @@ use App\Http\Requests;
 class InfoController extends Controller
 {
     public function index($type, $id) {
-        // $_params = $request->all();
-        // var_dump($type);
-        // var_dump($id);
-        // var_dump(session('token'));
         $_result = curlPost(
                     'http://120.25.218.156:12001/info/130/',
-                    json_encode(['token' => session('token'), 'type' => $type, 'id' => $id])
+                    json_encode(['token' => md5('123456'), 'type' => $type, 'id' => $id])
                 );
-        // var_dump($_result);
-        return view('info.index', ['data' => $_result['data'][0]]);
+        if($_result['ok'] === true) {
+            return view('info.index', ['title' => $type==1?'家族简介':'史料','data' => $_result['data'][0]]);
+        }
+        return view('info.index', ['title' => $type==1?'家族简介':'史料']);
     }
 }
