0760c2954b2ae6aa9c7055c15bf0a366cbc2770c
zhou3388232 <2482891006@qq.com>
2017-01-20 10:52:24 +0800

    info

---
+++ resources/views/info/index.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="renderer" content="webkit">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>亲缘</title>
+    <link rel="stylesheet" type="text/css" href="//at.alicdn.com/t/font_ds1jqxkqnneqxgvi.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('/css/rest.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('/css/main.css') }}">
+    <script type="text/javascript" src="{{ asset('/js/jquery-2.1.4.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('/js/jquery.cookie.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('/js/utility.js') }}"></script>
+  </head>
+  <body class="info">
+    <?php echo $data['title']; ?>
+    <?php echo $data['content']; ?>
+  <body>
+</html>
